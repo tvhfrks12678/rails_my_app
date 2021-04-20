2651883c67ea3bc571afaa06572711ef3ee9b17e
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-20 17:22:42 +0900

    full_title helperの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  # ページタイトルごとの完全なタイトルを返します
+  def full_title(page_title = '')
+    base_title = "韻クイズ"
+    return base_title if page_title.empty?
+
+    page_title + "|" + base_title
+  end
 end
