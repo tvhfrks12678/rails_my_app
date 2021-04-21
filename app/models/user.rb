6bb882d3989506ab340b6500232641ca6c2e45eb
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-21 17:18:50 +0900

    feat: Userモデルのemailのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   validates :profile, length: { maximum: 255 }
 end
