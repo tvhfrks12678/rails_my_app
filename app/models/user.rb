a3871861b1fa1acd8bd33b0ab21affd4f7297b37
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-21 18:52:32 +0900

    feat: Userモデルにsecure passwordsの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :profile, length: { maximum: 255 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
