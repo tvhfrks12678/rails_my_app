07e671cdd77e04c60cbf8670384d4fb7729beb8b
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-21 10:19:32 +0900

    Userモデルのテストの追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:name) { |n| "tarou-#{n}"}
+    sequence(:email) { |n| "tester#{n}@example.com"}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "name" do
+    context 'validation' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_most(50) }
+    end
+  end
+
+  describe "email" do
+    context 'validation' do
+      it { is_expected.to validate_presence_of(:email) }
+      it { should validate_length_of(:email).is_at_most(255) }
+    end
+  end
+
+  describe "profile" do
+    context "validation" do
+      it { should validate_length_of(:profile).is_at_most(255)}
+    end
+  end
+end
