74817f8eb2fb71be00d0ff720d5a9a39c047437f
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-21 11:38:14 +0900

    test: サイトのレイアウトの確認のテストの追加

---
+++ spec/system/site_layouts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "SiteLayouts", type: :system do
+  include ApplicationHelper
+
+  scenario 'layout links' do
+    visit root_path
+    click_link 'Home'
+    expect(current_path).to eq root_path
+    expect(page.title).to eq full_title
+    click_link 'Help'
+    expect(current_path).to eq help_path
+    expect(page.title).to eq full_title("Help")
+    click_link 'Contact'
+    expect(current_path).to eq contact_path
+    expect(page.title).to eq full_title('Contact')
+  end
+end
