64f0d4efbe425657836d04b742d571dc1420d88c
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-22 10:07:18 +0900

    feat: User登録用のStrong Parametersの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    return render 'new' unless @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
