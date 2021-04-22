d054a935f6fd0c8dc8b9e9be5a54912707852b5f
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-22 14:58:34 +0900

    test: ユーザー登録成功時のテストの追加

--- spec/system/users_signups_spec.rb
+++ spec/system/users_signups_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersSignups', type: :system do
-  scenario 'invalid signup information' do
+  scenario 'User perform sign-up' do
+    # invalid signup informatio
     visit root_path
     expect do
       all_items_invalid_user = User.new(name: '', email: 'invalid@email', password: 'foo', password_confirmation: 'bar')
@@ -10,6 +11,16 @@ RSpec.describe 'UsersSignups', type: :system do
         expect_signup_all_items_errors
       end
     end.to_not change(User, :count)
+
+    # valid signup information
+    expect do
+      valid_user = User.new(name: 'tarou', email: 'tarou@example.com', password: 'foobar',
+                            password_confirmation: 'foobar')
+      create_user_account(valid_user)
+      aggregate_failures do
+        expect_signup_success
+      end
+    end.to change(User, :count).by(1)
   end
 
   def create_user_account(user)
@@ -22,6 +33,7 @@ RSpec.describe 'UsersSignups', type: :system do
   end
 
   def expect_signup_all_items_errors
+    expect(current_path).to eq users_path
     within '#error_explanation' do
       expect(page).to have_selector '.alert-danger', text: 'エラー'
       user_input_items = %w[Name Email Password Password\ confirmation]
@@ -29,5 +41,10 @@ RSpec.describe 'UsersSignups', type: :system do
         expect(page).to have_content user_input_item
       end
     end
+  end
+
+  def expect_signup_success
+    expect(current_path).to eq user_path(User.last)
+    expect(page).to have_css '.alert-success'
   end
 end
