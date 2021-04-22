114da2c0c011c33eef7ae6fe0e8ab5de8093bdd2
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-22 18:13:50 +0900

    feat: login後の表示処理の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  MSG_LOG_IN_FAILURE = 'EmaiまたはPasswordが正しくありません。'.freeze
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    return if user&.authenticate(params[:session][:password])
+
+    flash.now[:danger] = MSG_LOG_IN_FAILURE
+    render 'new'
+  end
+
+  def destroy
   end
 end
