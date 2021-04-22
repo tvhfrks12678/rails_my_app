d42a0ec2f60ee4662f37211e358f9dc4e2038842
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-22 15:37:48 +0900

    feat: Session Controllerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get '/rhyme', to: 'static_pages#rhyme'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
