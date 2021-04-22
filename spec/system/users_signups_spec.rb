3ec688c42205708731660262732dd1cb80c58bfb
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-22 13:35:53 +0900

    refactor: 無効なユーザー登録に対するテストをリファクタリング

--- spec/system/users_signups_spec.rb
+++ spec/system/users_signups_spec.rb
@@ -3,21 +3,32 @@ require 'rails_helper'
 RSpec.describe 'UsersSignups', type: :system do
   scenario 'invalid signup information' do
     visit root_path
+
     expect do
-      visit users_new_path
-      fill_in 'user_name',	with: ''
-      fill_in 'user_email', with: 'invalid@mail'
-      fill_in 'user_password',	with: 'foo'
-      fill_in 'user_password_confirmation',	with: 'bar'
-      click_button 'アカウント作成'
+      all_items_invalid_user = User.new(name: '', email: 'invalid@email', password: 'foo', password_confirmation: 'bar')
+      create_user_account(all_items_invalid_user)
       aggregate_failures do
-        expect(current_path).to eq users_path
-        expect(page).to have_selector '.alert-danger', text: 'エラー'
-        expect(page).to have_selector '#error_explanation', text: 'Email'
-        expect(page).to have_selector '#error_explanation', text: 'Email'
-        expect(page).to have_selector '#error_explanation', text: 'Password'
-        expect(page).to have_selector '#error_explanation', text: 'Password confirmation'
+        expect_signup_all_items_errors
       end
     end.to_not change(User, :count)
+  end
+
+  def create_user_account(user)
+    visit users_new_path
+    fill_in 'user_name',	with: user.name
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password',	with: user.password
+    fill_in 'user_password_confirmation',	with: user.password_confirmation
+    click_button 'アカウント作成'
+  end
+
+  def expect_signup_all_items_errors
+    within '#error_explanation' do
+      expect(page).to have_selector '.alert-danger', text: 'エラー'
+      user_input_items = %w[Name Email Password Password\ confirmation]
+      user_input_items.each do |user_input_item|
+        expect(page).to have_content user_input_item
+      end
+    end
   end
 end
