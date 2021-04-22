7ef5977f897d44ead11bdf2003dcba6936d71fdf
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-22 13:56:13 +0900

    feat: 「韻とは？」ページを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def rhyme
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
+  get '/rhyme', to: 'static_pages#rhyme'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
