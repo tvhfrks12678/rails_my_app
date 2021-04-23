7fd94bb51d2b53ae216b233d42e2fcf1c7e8e1e8
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-23 17:41:12 +0900

    feat: QuizモデルにValidationの追加

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,6 @@
 class Quiz < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :commentary, length: { maximum: 255 }
 end
