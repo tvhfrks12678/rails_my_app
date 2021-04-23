1026b90da33ec386dcbd3c5385e36b8bd88b746c
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-23 11:14:00 +0900

    test: ログイン処理のテストの追加

--- spec/system/users_logins_spec.rb
+++ spec/system/users_logins_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersLogins', type: :system do
-  scenario 'login with invalid information' do
+  scenario 'log in' do
     visit root_path
+    # login with invalid information
     invalid_user = build(:user, email: 'invalid@email', password: 'foo')
     log_in_as(invalid_user)
     expect_log_in_failure
-    # 移動先にフラッシュメッセージが表示されないかのチェック
+
+    # 移動先にフラッシュメッセージが表示されないか
     click_link 'Home'
     expect(page).to_not have_css '.alert'
+
+    # login with valid email/invalid password
+    valid_user = create(:user)
+    valid_email_invalid_password_user = build(:user, email: valid_user.email, password: 'foo')
+    log_in_as(valid_email_invalid_password_user)
+    expect_log_in_failure
+
+    # login with valid information
+    log_in_as(valid_user)
+    expect_log_in_success(valid_user)
   end
 
   def log_in_as(user)
@@ -21,5 +33,16 @@ RSpec.describe 'UsersLogins', type: :system do
   def expect_log_in_failure
     expect(current_path).to eq login_path
     expect(page).to have_css '.alert-danger'
+  end
+
+  def expect_log_in_success(user)
+    expect(current_path).to eq user_path(user)
+    within 'header > div > nav' do
+      expect(page).to_not have_link 'Log in'
+      header_link_words = %w[投稿 Profile Setting Log\ out]
+      header_link_words.each do |header_link_word|
+        expect(page).to have_link header_link_word
+      end
+    end
   end
 end
