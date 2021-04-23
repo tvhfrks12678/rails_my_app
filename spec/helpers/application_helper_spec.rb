cc70042698e2773e63855683effa8a1c796d1ad6
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-23 12:49:26 +0900

    style: it、contextを英語に修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,27 +2,17 @@
 
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelperHelper. For example:
-#
-# describe ApplicationHelperHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#full_title(page_title = \'\')' do
     let(:base_title) { '韻クイズ' }
-    context 'page_titleが空白の時' do
-      it '|を含まないTitleをreturnする' do
+    context 'when page_title is blank' do
+      it 'should not include |' do
         expect(full_title).to eq base_title
       end
     end
 
-    context 'page_titleがAboutの時' do
-      it '|を含むTitleをreturnする' do
+    context 'when page_title is About' do
+      it 'should include |' do
         expect(full_title('About')).to eq "About | #{base_title}"
       end
     end
