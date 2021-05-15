d4c39e531eae537f3956febe883399620b7a48ab
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-05-15 16:35:15 +0900

    feat: クイズの回答を表示するための情報の取得処理の追加 #7

--- app/controllers/answer_judgements_controller.rb
+++ app/controllers/answer_judgements_controller.rb
@@ -1,17 +1,6 @@
 class AnswerJudgementsController < ApplicationController
-  # MSG
-  MSG_ANSWER_CORRECT = '正解'.freeze
-  MSG_ANSWER_INCORRECT = '不正解'.freeze
-
   def create
-    answers = Answer.get(quiz_id: params[:quiz][:id])
-
-    if Answer.correct?(answers, params[:select_choices])
-      flash.now[:success] = MSG_ANSWER_CORRECT
-      return redirect_to quizzes_path
-    end
-
-    flash.now[:danger] = MSG_ANSWER_INCORRECT
+    @answer = Answer.get(quiz_id: params[:quiz][:id], select_choice_ids: params[:select_choices])
     redirect_to quizzes_path
   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,44 +1,72 @@
 class Answer
   include ActiveModel::Model
 
-  attr_reader :rhyme, :choice_ids
+  # quiz: Quizモデル
+  # combinations: AnswerCombinationモデルのリスト
+  attr_reader :quiz, :result, :combinations
 
-  validates :rhyme, presence: true
-  validates :choice_ids, presence: true
+  validates :quiz, presence: true
+  validates :result, inclusion: { in: [true, false] }
+  validates :combinations, presence: true
 
   # SQL
-  SQL_COLUMS_CHOICES = 'choices.id, rhymes.content as rhyme_content'.freeze
+  SQL_COLUMS_GET = 'choices.id AS Choice_id, rhymes.content  AS rhyme_content'.freeze
 
-  def initialize(rhyme:, choice_ids:)
-    @rhyme = rhyme
-    @choice_ids = choice_ids
+  def initialize(quiz:, result:, combinations:)
+    @quiz = quiz
+    @result = result
+    @combinations = combinations
   end
 
-  def correct?(select_choice_ids)
-    (choice_ids - select_choice_ids).empty?
-  end
+  class << self
+    def get(quiz_id:, select_choice_ids:)
+      quiz = Quiz.select(:id, :commentary).find(quiz_id)
+      choices = quiz.choices.joins(:rhyme).select(SQL_COLUMS_GET)
 
-  # answers:Answerモデルのリスト 
-  def self.correct?(answers, select_choice_ids)
-    answers.each do |answer|
-      # return true if (answer.choice_ids - select_choice_ids).empty?
-      return true if answer.correct?(select_choice_ids)
-    end
-    false
-  end
+      answer_combinations = get_answer_combinations(choices)
 
-  def self.get(quiz_id:)
-    choices = Choice.joins(:rhyme).where(quiz_id: quiz_id).select(SQL_COLUMS_CHOICES)
-    rhyme_contents = choices.map(&:rhyme_content).uniq
-    answers = []
-
-    rhyme_contents.each_with_index do |rhyme_content, idx|
-      choice_choice_ids = choices
-                          .select { |choice| choice.rhyme_content == rhyme_content }
-                          .map { |item| item.id.to_s }
-      answers[idx] = Answer.new(rhyme: rhyme_content, choice_ids: choice_choice_ids)
+      Answer.new(quiz: quiz,
+                 result: correct?(answer_combinations, select_choice_ids),
+                 combinations: answer_combinations)
     end
 
-    answers
+    private
+
+    def get_answer_combinations(choices)
+      rhyme_contents = choices.map(&:rhyme_content).uniq
+      answer_combinations = []
+
+      rhyme_contents.each_with_index do |rhyme_content, idx|
+        choice_ids = choices
+                     .select { |choice| choice.rhyme_content == rhyme_content }
+                     .map { |item| item.choice_id.to_s }
+        answer_combinations[idx] = AnswerCombination.new(rhyme: rhyme_content, choice_ids: choice_ids)
+      end
+      answer_combinations
+    end
+
+    # answers:AnswerCombinationモデルのリスト
+    def correct?(answer_combinations, select_choice_ids)
+      answer_combinations.each do |answer_combination|
+        return true if answer_combination.correct?(select_choice_ids)
+      end
+      false
+    end
+  end
+
+  class AnswerCombination
+    include ActiveModel::Model
+
+    validates :rhyme, presence: true
+    validates :choice_ids, presence: true
+
+    def initialize(rhyme:, choice_ids:)
+      @rhyme = rhyme
+      @choice_ids = choice_ids
+    end
+
+    def correct?(select_choice_ids)
+      (@choice_ids - select_choice_ids).empty?
+    end
   end
 end
