997613ab334b7c09678ad778a81518663b1ffbe9
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-05-20 15:21:13 +0900

    feat: 回答ボタンクリック後に選択肢の下に母音を表示する処理を修正 #7
    ・母音の表示色の修正
    ・メソッドの抽出
    ・変数名の変更

--- app/models/rhyme.rb
+++ app/models/rhyme.rb
@@ -1,5 +1,5 @@
 class Rhyme < ApplicationRecord
   has_one :choice, dependent: :nullify
-  VALID_CONTENT_REGEX = /\A[あいうえお]+\z/.freeze
+  VALID_CONTENT_REGEX = /\A[あいうえおaiueo]+\z/.freeze
   validates :content, presence: true, length: { maximum: 30 }, format: { with: VALID_CONTENT_REGEX }, uniqueness: true
 end
