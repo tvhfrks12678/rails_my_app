15dd6b41457e0fc155921bdc460eabf63e2df6e8
tvhfrks12678 <tvhfrks12678@gmail.com>
2022-01-22 14:21:38 +0900

    refactor: 回答表示処理の修正

--- app/view_models/answers/answer.rb
+++ app/view_models/answers/answer.rb
@@ -33,16 +33,12 @@ module ViewModels
       # @param [Quiz] quiz
       # @return [Array<AnswerCombination>]
       def get_answer_combinations(quiz)
-        choices = quiz.choices.preload(:rhyme)
+        choices = quiz.choices.preload(:rhyme).where.not(rhyme_id: nil)
 
         answer_combinations = []
 
         choices.each do |choice|
-          choice_rhyme = choice&.rhyme&.content
-          choice_id = choice.id.to_s
-          next if choice_rhyme.blank?
-
-          answer_combinations = add_answer_combinations(choice_rhyme, choice_id, answer_combinations)
+          answer_combinations = add_answer_combinations(choice, answer_combinations)
         end
 
         answer_combinations
@@ -50,10 +46,12 @@ module ViewModels
 
       # 母音と選択肢の組み合わせを追加する
       #
-      # @param [String] choice_rhyme
-      # @param [String] choice_id
-      # @return [Array<AnswerCombination>]
-      def add_answer_combinations(choice_rhyme, choice_id, answer_combinations)
+      # @param [Choice] choice
+      # @param [Array<AnswerCombination>] answer_combinations
+      # @return [Array<AnswerCombination>] answer_combinations
+      def add_answer_combinations(choice, answer_combinations)
+        choice_rhyme = choice.rhyme.content
+        choice_id = choice.id.to_s
         answer_combinations.each do |answer_combination|
           if answer_combination.rhyme == choice_rhyme
             answer_combination.choice_ids << choice_id
@@ -63,14 +61,6 @@ module ViewModels
 
         answer_combinations << ViewModels::Answers::AnswerCombination.new(rhyme: choice_rhyme,
                                                                           choice_ids: [choice_id])
-      end
-
-      # 母音をローマ字にする
-      #
-      # @param [String] rhyme_content ひらがな、カタカナ、ローマ字混ざりの母音
-      # @return [String] ローマ字のみの母音
-      def get_alphabet_rhyme(rhyme_content)
-        rhyme_content.gsub(/[あア]/, 'a').gsub(/[いイ]/, 'i').gsub(/[うウ]/, 'u').gsub(/[えエ]/, 'e').gsub(/[おオ]/, 'o')
       end
 
       # クイズの回答が正解か判定する
--- app/view_models/answers/answer_combination.rb
+++ app/view_models/answers/answer_combination.rb
@@ -8,11 +8,25 @@ module ViewModels
       validates :rhyme, presence: true
       validates :choice_ids, presence: true
 
+      ROMAJI_TO_HIRANAGA_AND_KATAKANA = { a: 'あア', i: 'いイ', u: 'うウ', e: 'えエ', o: 'おオ' }.freeze
+
       # @param [String] rhyme
       # @param [Array<String>] choice_ids
       def initialize(rhyme:, choice_ids:)
         @rhyme = rhyme
         @choice_ids = choice_ids
+      end
+
+      # 母音をローマ字にする
+      #
+      # @return [String] ローマ字のみの母音
+      def romaji_rhyme
+        romaji_rhyme = rhyme.dup
+        ROMAJI_TO_HIRANAGA_AND_KATAKANA.each do |key, value|
+          romaji_rhyme.gsub!(/[#{value}]/, key.to_s)
+        end
+
+        romaji_rhyme
       end
 
       # 選ばれた選択肢が正しいか判定する
